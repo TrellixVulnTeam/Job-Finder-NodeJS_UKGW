5bd48a24e533ec96978d0a72eb44ee4c859583a4
Mariano Carlos <Mariano.silva@ufvjm.edu.br>
2022-07-17 12:59:39 -0300

    bd resolved
    Aqui os problemas com o banco de dados foram resolvidos, anteriormente não estava conseguindo enviar os dados.

--- app.js
+++ app.js
@@ -3,24 +3,25 @@ const app        = express();
 const db         = require('./dataBase/connection');
 const bodyParser = require('body-parser');
 
-const PORT = 3000;
+const PORT = 5058;
 
 app.listen(PORT, function(){
-    console.log(`O express esta rodando na porta ${PORT}.`);
-    
+    console.log(`O express esta rodando na porta ${PORT}.`)
+
 });
 //body parser
 app.use(bodyParser.urlencoded({extended: false}));
+//app.use(bodyParser.json());
 
 //Conexão com o banco
 db
- .authenticate()
- .then(()=>{
-     console.log("Conectou ao banco com sucesso")
- })
- .catch(err =>{
-    console.log("Ocorreu um erro ao conectar " + err);
- });
+  .authenticate()
+  .then(() => {
+    console.log("Conectou ao banco com sucesso");
+  })
+  .catch(err => {
+    console.log("Ocorreu um erro ao conectar", err);
+  });
 
 
 
